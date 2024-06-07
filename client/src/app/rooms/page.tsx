25359ee4e82380bb4f1800ad65b8db8706411ce4
Brock Phillis <Brockphillis1992@gmail.com>
2024-06-08 09:01:43 +1000

    fix auth server url conditions

--- client/src/app/rooms/page.tsx
+++ client/src/app/rooms/page.tsx
@@ -1,15 +1,23 @@
 import { RoomManager } from "@/modules/RoomManager";
 import { get_Rooms } from "@/helpers/async_roomHelpers";
 import { AuthContextProvider } from "@/context/AuthContext";
+import { NextResponse } from "next/server";
 
 export default async function Page() {
   const { rooms } = await get_Rooms(true);
 
-  return (
-    <AuthContextProvider
-      authServerUrl={process.env.NEXT_PUBLIC_AUTH_SERVER_URL!}
-    >
-      <RoomManager initialRooms={rooms} />
-    </AuthContextProvider>
-  );
+  if (process.env.NEXT_PUBLIC_AUTH_SERVER_URL) {
+    return (
+      <AuthContextProvider
+        authServerUrl={process.env.NEXT_PUBLIC_AUTH_SERVER_URL}
+      >
+        <RoomManager initialRooms={rooms} />
+      </AuthContextProvider>
+    );
+  } else {
+    return NextResponse.json(
+      { error: "Authorization Server Not Found" },
+      { status: 401 }
+    );
+  }
 }
--- client/src/wrappers/MasterWrapper/index.tsx
+++ client/src/wrappers/MasterWrapper/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { UIContextProvider } from "@/context/UIContext";
 import WelcomeBackToast from "@/components/Toast/WelcomeBackToast";
 import { AuthContextProvider } from "@/context/AuthContext";
+import { NextResponse } from "next/server";
 
 const MasterWrapper = ({
   children,
@@ -14,14 +15,24 @@ const MasterWrapper = ({
 }) => {
   const trigger = false;
 
-  return (
-    <AuthContextProvider user={user}>
-      <UIContextProvider>
-        {children}
-        {useWelcomeMessage && <WelcomeBackToast trigger={trigger} />}
-      </UIContextProvider>
-    </AuthContextProvider>
-  );
+  if (process.env.NEXT_PUBLIC_AUTH_SERVER_URL) {
+    return (
+      <AuthContextProvider
+        user={user}
+        authServerUrl={process.env.NEXT_PUBLIC_AUTH_SERVER_URL}
+      >
+        <UIContextProvider>
+          {children}
+          {useWelcomeMessage && <WelcomeBackToast trigger={trigger} />}
+        </UIContextProvider>
+      </AuthContextProvider>
+    );
+  } else {
+    return NextResponse.json(
+      { error: "Authorization Server Not Found" },
+      { status: 401 }
+    );
+  }
 };
 
 export default MasterWrapper;
