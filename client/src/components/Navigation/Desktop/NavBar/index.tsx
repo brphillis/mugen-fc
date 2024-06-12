2739049ce02186dab7d67363198236672df41467
Brock Phillis <Brockphillis1992@gmail.com>
2024-06-12 18:42:15 +1000

    fix deploy yamls cicd

--- client/src/components/Navigation/Desktop/NavBar/index.tsx
+++ client/src/components/Navigation/Desktop/NavBar/index.tsx
@@ -37,7 +37,7 @@ const Navbar = ({ user }: Props) => {
           href="/"
           className="w-[260px] max-lg:w-full text-2xl text-brand-white"
         >
-          MFC3
+          MFC100
         </Link>
 
         <NavBarLinks />
